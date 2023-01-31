5e40939bae460ad6baad541bf585eb30a4ab5db3
Wouter Saelens <wouter.saelens@gmail.com>
2023-01-31 19:33:19 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ setuptools.setup(
     ],
     python_requires=">=3.8",
     install_requires=[
-        "torch=1.12.1",  # --extra-index-url https://download.pytorch.org/whl/cu113
-        "torch-scatter"  # --find-links https://data.pyg.org/whl/torch-1.12.1+cu113.html
+        "torch==1.12.1",  # --extra-index-url https://download.pytorch.org/whl/cu113
+        "torch-scatter",  # --find-links https://data.pyg.org/whl/torch-1.12.1+cu113.html
         "scanpy",
         "seaborn",
         "pybedtools",
