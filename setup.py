b3ca72e12a310967f3a0baaec0d7e4b4f0e48d09
Wouter Saelens <wouter.saelens@gmail.com>
2023-02-24 09:04:26 +0100

    add fisher to requires

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setuptools.setup(
         "seaborn",
         "pybedtools",
         "Cython",
+        "fisher",
     ],
     extras_require={
         "full": [],
@@ -54,7 +55,7 @@ setuptools.setup(
             "jupytext",
             "pytest",
             "statsmodels",
-            "faiss-cpu",
+            # "faiss-cpu",
         ],
     },
 )
