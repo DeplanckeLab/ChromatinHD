8b1a752bb72ddb17ccb1bd210bc0f1e779777097
Wouter Saelens <wouter.saelens@gmail.com>
2023-03-22 10:24:01 +0100

    update prediction

--- src/chromatinhd/scoring/prediction/__init__.py
+++ src/chromatinhd/scoring/prediction/__init__.py
@@ -132,7 +132,9 @@ import xarray as xr
 
 
 class Scorer2:
-    def __init__(self, models, folds, loaders, outcome, gene_ids, device="cuda"):
+    def __init__(
+        self, models, folds, loaders, outcome, gene_ids, cell_ids, device="cuda"
+    ):
         assert len(models) == len(folds)
         self.outcome = outcome
         self.models = models
@@ -140,6 +142,7 @@ class Scorer2:
         self.folds = folds
         self.loaders = loaders
         self.gene_ids = gene_ids
+        self.cell_ids = cell_ids
 
     def score(
         self,
@@ -185,6 +188,8 @@ class Scorer2:
             coords=[model_dim, phases_dim, genes_dim, design_dim],
         )
 
+        cellgeneeffects = []
+
         n_fragments_lost = xr.DataArray(
             0,
             coords=[model_dim, phases_dim, genes_dim, design_dim],
@@ -198,24 +203,25 @@ class Scorer2:
                 fold["genes_all"],
             )
             gene_ix_mapper = fold["gene_ix_mapper"]
+            cell_ix_mapper = fold["cell_ix_mapper"]
+            cells_dim = self.cell_ids[fold["cells_all"]]
+            assert cells_dim.name == "cell"
 
             n_design = filterer.setup_next_chunk()
             # create transcriptome_predicted
             transcriptome_predicted_ = [
-                np.zeros([self.outcome.shape[0], len(genes_dim)])
-                for i in range(n_design)
+                np.zeros([len(cells_dim), len(genes_dim)]) for i in range(n_design)
             ]
             n_fragments_lost_cells = [
-                np.zeros([self.outcome.shape[0], len(genes_dim)])
-                for i in range(n_design)
+                np.zeros([len(cells_dim), len(genes_dim)]) for i in range(n_design)
             ]
 
             # infer and score
             with torch.no_grad():
                 # infer
                 model = model.to(self.device)
-                for data in tqdm.tqdm(self.loaders):
-                    # for data in self.loaders:
+                # for data in tqdm.tqdm(self.loaders):
+                for data in self.loaders:
                     data = data.to(self.device)
 
                     fragments_oi = filterer.filter(data)
@@ -226,49 +232,70 @@ class Scorer2:
                         )
                     ):
                         transcriptome_predicted_[design_ix][
-                            np.ix_(data.cells_oi, gene_ix_mapper[data.genes_oi])
-                        ] = (predicted.detach().cpu().numpy())
+                            np.ix_(
+                                cell_ix_mapper[data.cells_oi],
+                                gene_ix_mapper[data.genes_oi],
+                            )
+                        ] = (
+                            predicted.detach().cpu().numpy()
+                        )
 
                         n_fragments_lost_cells[design_ix][
-                            np.ix_(data.cells_oi, gene_ix_mapper[data.genes_oi])
-                        ] = (n_fragments_oi_mb.detach().cpu().numpy())
+                            np.ix_(
+                                cell_ix_mapper[data.cells_oi],
+                                gene_ix_mapper[data.genes_oi],
+                            )
+                        ] = (
+                            n_fragments_oi_mb.detach().cpu().numpy()
+                        )
 
                     self.loaders.submit_next()
 
-                # score
-                for phase, (cells, genes) in fold["phases"].items():
-                    phase_ix = phases_dim.tolist().index(phase)
+            model = model.to("cpu")
 
-                    outcome_phase = self.outcome.numpy()[np.ix_(cells, genes)]
+            # score
+            for phase, (cells, genes) in fold["phases"].items():
+                phase_ix = phases_dim.tolist().index(phase)
 
-                    transcriptome_predicted_full__ = transcriptome_predicted_full[
-                        model_ix
-                    ][np.ix_(cells, genes)]
+                outcome_phase = self.outcome.numpy()[np.ix_(cells, genes)]
 
-                    genes = gene_ix_mapper[genes]
+                transcriptome_predicted_full__ = transcriptome_predicted_full[model_ix][
+                    np.ix_(cells, genes)
+                ]
 
-                    for design_ix in range(n_design):
-                        transcriptome_predicted__ = transcriptome_predicted_[design_ix][
-                            np.ix_(cells, genes)
-                        ]
+                genes = gene_ix_mapper[genes]
+                cells = cell_ix_mapper[cells]
 
-                        cor_gene = chd.utils.paircor(
-                            outcome_phase, transcriptome_predicted__
-                        )
+                for design_ix in range(n_design):
+                    transcriptome_predicted__ = transcriptome_predicted_[design_ix][
+                        np.ix_(cells, genes)
+                    ]
 
-                        cors.values[model_ix, phase_ix, design_ix] = cor_gene.mean()
-                        genecors.values[model_ix, phase_ix, genes, design_ix] = cor_gene
+                    cor_gene = chd.utils.paircor(
+                        outcome_phase, transcriptome_predicted__
+                    )
 
-                        n_fragments_lost.values[
-                            model_ix, phase_ix, genes, design_ix
-                        ] = n_fragments_lost_cells[design_ix][np.ix_(cells, genes)].sum(
-                            0
-                        )
+                    cors.values[model_ix, phase_ix, design_ix] = cor_gene.mean()
+                    genecors.values[model_ix, phase_ix, genes, design_ix] = cor_gene
 
-                        effect = (
-                            transcriptome_predicted__ - transcriptome_predicted_full__
-                        ).mean(0)
-                        geneffects[model_ix, phase_ix, genes, design_ix] = effect
+                    n_fragments_lost.values[
+                        model_ix, phase_ix, genes, design_ix
+                    ] = n_fragments_lost_cells[design_ix][np.ix_(cells, genes)].sum(0)
+
+                    effect = (
+                        transcriptome_predicted__ - transcriptome_predicted_full__
+                    ).mean(0)
+                    geneffects[model_ix, phase_ix, genes, design_ix] = effect
+
+            cellgeneeffect = xr.DataArray(
+                np.stack(transcriptome_predicted_, 0)
+                - transcriptome_predicted_full[model_ix][
+                    np.ix_(fold["cells_all"], fold["genes_all"])
+                ][None, ...],
+                coords=[design_dim, cells_dim, genes_dim],
+            )
+
+            cellgeneeffects.append(cellgeneeffect)
 
         scores = xr.Dataset({"cor": cors})
         genescores = xr.Dataset(
@@ -284,152 +311,4 @@ class Scorer2:
 
         self.scores = scores.mean("model")
         self.genescores = genescores.mean("model")
-
-        return scores, genescores
-
-
-class Scorer3:
-    def __init__(self, models, folds, loaders, outcome, gene_ids, device="cuda"):
-        assert len(models) == len(folds)
-        self.outcome = outcome
-        self.models = models
-        self.device = device
-        self.folds = folds
-        self.loaders = loaders
-        self.gene_ids = gene_ids
-
-    def score(
-        self,
-        loader_kwargs=None,
-        return_prediction=False,
-        transcriptome_predicted_full=None,
-        filterer=None,
-    ):
-        """
-        gene_ids: mapping of gene ix to gene id
-        """
-
-        n_fragments = {}
-
-        folds = self.folds
-
-        if loader_kwargs is None:
-            loader_kwargs = {}
-
-        next_task_sets = []
-        for fold in folds:
-            next_task_sets.append({"tasks": fold["minibatches"]})
-        next_task_sets[0]["loader_kwargs"] = loader_kwargs
-        self.loaders.initialize(next_task_sets=next_task_sets)
-
-        phases_dim = pd.Index(folds[0]["phases"], name="phase")
-        genes_dim = self.gene_ids
-        design_dim = filterer.design.index
-        model_dim = pd.Index(np.arange(len(folds)), name="model")
-
-        cors = xr.DataArray(
-            0.0,
-            coords=[model_dim, phases_dim, design_dim],
-        )
-
-        genecors = xr.DataArray(
-            0.0,
-            coords=[model_dim, phases_dim, genes_dim, design_dim],
-        )
-
-        geneffects = xr.DataArray(
-            0.0,
-            coords=[model_dim, phases_dim, genes_dim, design_dim],
-        )
-
-        genelost = xr.DataArray(
-            0,
-            coords=[model_dim, phases_dim, genes_dim, design_dim],
-        )
-        genetotal = xr.DataArray(
-            0,
-            coords=[model_dim, phases_dim, genes_dim, design_dim],
-        )
-
-        for model_ix, (model, fold) in tqdm.tqdm(
-            enumerate(zip(self.models, folds)), leave=False, total=len(self.models)
-        ):
-            assert len(genes_dim) == fold["genes_all"]
-            gene_ix_mapper = fold["gene_ix_mapper"]
-
-            n_design = filterer.setup_next_chunk()
-            # create transcriptome_predicted
-            transcriptome_predicted_ = [
-                np.zeros(self.outcome.shape) for i in range(n_design)
-            ]
-            n_fragments_ = [np.zeros(self.outcome.shape) for i in range(n_design)]
-
-            # infer and score
-            with torch.no_grad():
-                # infer
-                model = model.to(self.device)
-                for data in tqdm.tqdm(self.loaders):
-                    # for data in self.loaders:
-                    data = data.to(self.device)
-
-                    fragments_oi = filterer.filter(data)
-
-                    for design_ix, (predicted, n_lost, n_total) in enumerate(
-                        model.forward_multiple(data, fragments_oi)
-                    ):
-                        transcriptome_predicted_[design_ix][
-                            np.ix_(data.cells_oi, gene_ix_mapper[data.genes_oi])
-                        ] = (predicted.detach().cpu().numpy())
-
-                        genelost.values[
-                            model_ix, gene_ix_mapper[data.genes_oi], design_ix
-                        ] += (n_lost.detach().cpu().numpy())
-                        genetotal.values[
-                            model_ix, gene_ix_mapper[data.genes_oi], design_ix
-                        ] += (n_total.detach().cpu().numpy())
-
-                    self.loaders.submit_next()
-
-                # score
-                for phase, (cells, genes) in fold["phases"].items():
-                    phase_ix = phases_dim.tolist().index(phase)
-
-                    outcome_phase = self.outcome.numpy()[
-                        np.ix_(cells, gene_ix_mapper[genes])
-                    ]
-
-                    transcriptome_predicted_full__ = transcriptome_predicted_full[
-                        model_ix
-                    ][np.ix_(cells, gene_ix_mapper[genes])]
-                    for design_ix in range(n_design):
-                        transcriptome_predicted__ = transcriptome_predicted_[design_ix][
-                            np.ix_(cells, gene_ix_mapper[genes])
-                        ]
-
-                        cor_gene = chd.utils.paircor(
-                            outcome_phase, transcriptome_predicted__
-                        )
-
-                        cors.values[model_ix, phase_ix, design_ix] = cor_gene.mean()
-                        genecors.values[
-                            model_ix, phase_ix, gene_ix_mapper[genes], design_ix
-                        ] = cor_gene
-
-                        effect = (
-                            transcriptome_predicted__ - transcriptome_predicted_full__
-                        ).mean(0)
-                        geneffects[
-                            model_ix, phase_ix, gene_ix_mapper[genes], design_ix
-                        ] = effect
-
-        scores = xr.Dataset({"cor": cors})
-        genescores = xr.Dataset(
-            {
-                "cor": genecors,
-                "effect": geneffects,
-                "lost": genelost,
-                "total": genetotal,
-            }
-        )
-
-        return scores, genescores
+        self.cellgeneeffects = cellgeneeffects
