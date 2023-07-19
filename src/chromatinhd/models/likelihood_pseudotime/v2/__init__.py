09b053de54b17d41fb62b56190141e6c0fe8a520
Wouter Saelens <wouter.saelens@gmail.com>
2023-07-19 11:27:02 +0200

    add v2

---
+++ src/chromatinhd/models/likelihood_pseudotime/v2/__init__.py
@@ -0,0 +1,207 @@
+import math
+import torch
+import torch_scatter
+import dataclasses
+import numpy as np
+import tqdm.auto as tqdm
+
+# HybridModel = Model with both sparse and dense parameters instead of just one set of parameters. This is custom made by me but based on code from the internet
+from chromatinhd.models import HybridModel
+from chromatinhd.embedding import EmbeddingTensor
+from . import spline
+from .spline import DifferentialQuadraticSplineStack, TransformedDistribution
+
+
+class Decoder(torch.nn.Module):
+    def __init__(self, n_genes, n_delta_height):
+        print("---  Decoder.__init__()  ---")
+        super().__init__()
+
+        # the sparse gradient way
+        # initialize
+        # all function ending with a _ work inplace, so they will replace the
+        # .data is necessary here because self.delta_height_slope.weight is a tensor that has `requires_grad == True`
+        # but we don't want to track the gradients of the .zero_() operation, we actually want to set the data to zero
+        # we can do that by using the .data attribute, which gives you a view of the tensor where gradients are no longer tracked
+        # a view of a tensor is a tensor which accesses the same data (so the same memory) as the original tensor, without making a copy
+        self.delta_height_slope = EmbeddingTensor(n_genes, (n_delta_height,), sparse=True)
+        self.delta_height_slope.weight.data.zero_()
+
+        # do the same but for the "delta overall" slope
+        self.delta_overall_slope = EmbeddingTensor(n_genes, (1,), sparse=True)
+        self.delta_overall_slope.weight.data.zero_() 
+        # print(f"{self.delta_overall_slope.weight=}")
+        # TODO: check this variable
+
+    def forward(self, latent, genes_oi):
+        print("---  Decoder.forward()  ---")
+        # genes oi is only used to get the deltas
+        # we extract the overall slope for all genes because we have to do softmax later
+        delta_height_slope = self.delta_height_slope(genes_oi)
+        delta_overall_slope = self.delta_overall_slope.get_full_weight()
+
+        #! you will have to do some broadcasting here
+        # what we need is for each cell x gene x knot its delta
+        # and for each cell x gene its overall
+        delta_height_slope = delta_height_slope.unsqueeze(2)
+        delta_overall_slope = delta_overall_slope.unsqueeze(2)
+        latent = latent.unsqueeze(0).unsqueeze(0)
+
+        delta_height = delta_height_slope * latent
+
+        # https://pytorch.org/docs/stable/special.html#torch.special.expit
+        
+        delta_height = delta_height_slope/(1+torch.exp(-torch.exp(log_delta_height_scale) * latent - delta_height_shift))
+        delta_overall = delta_overall_slope * latent
+
+        return delta_height, delta_overall
+
+    def parameters_dense(self):
+        # this parameter is optimized for all genes, because of library size
+        # if one gene gets more accessible, all the other genes should get less accessible
+        # NOTE: this is not standard torch procedures, as torch typically only works with .parameters()
+        # We have to split the parameters in dense and sparse here because we want need two different torch optimizers for these two sets of parameters
+        return [self.delta_overall_slope.weight]
+
+    def parameters_sparse(self):
+        return [self.delta_height_slope.weight]
+
+
+# likelihoodresult = namedtuple("likelihoodresult", ["likelihood", "height_likelihood", "overall_likelihood"])
+# result = likelihoodresult(likelihood=torch.tensor(1), height_likelihood=torch.tensor(1), overall_likelihood=torch.tensor(1))
+# overall, height, total = result
+
+
+@dataclasses.dataclass
+class LikelihoodResult():
+    overall:torch.Tensor
+    height:torch.Tensor
+    total:torch.Tensor
+
+
+class Model(torch.nn.Module, HybridModel):
+    def __init__(self, fragments, latent, nbins=(128,), scale_likelihood=False, height_slope_p_scale=1.0):
+        super().__init__()
+        print("---  Model.__init__()  ---")
+
+        transform = DifferentialQuadraticSplineStack(nbins=nbins, n_genes=fragments.n_genes)
+        n_delta_height = sum(transform.split_deltas)
+
+        self.nf = TransformedDistribution(transform)
+        self.decoder = Decoder(fragments.n_genes, n_delta_height=n_delta_height)
+        self.n_total_genes = fragments.n_genes
+        self.n_total_cells = fragments.n_cells
+        self.scale_likelihood = scale_likelihood
+
+        # calculate library size for each cell
+        libsize = torch.bincount(fragments.mapping[:, 0], minlength=fragments.n_cells)
+
+        # calculate and store the baseline accessibility for each gene
+        # overall_baseline: [genes]
+        min_overall_baseline = 1e-5
+        overall_baseline = (torch.bincount(fragments.mapping[:, 1], minlength=fragments.n_genes) / fragments.n_cells / libsize.to(torch.float).mean())
+        overall_baseline = torch.log(min_overall_baseline + (1 - min_overall_baseline) * overall_baseline)
+
+        self.register_buffer("latent", latent)
+        self.register_buffer("libsize", libsize)
+        self.register_buffer("overall_baseline", overall_baseline)
+        self.register_buffer("height_slope_p_scale", torch.tensor(math.log(height_slope_p_scale)))
+        self.register_buffer("overall_slope_p_scale", torch.tensor(math.log(1.0)))
+
+    def forward_(self, cut_coordinates, latent, genes_oi, cut_local_cellxgene_ix, cut_localcellxgene_ix, cut_local_gene_ix, return_likelihood=False):
+        print("---  Model.forward_()  ---")
+
+        self.track = {}
+
+        # overall_delta: [cells, genes (all)]
+        # height_delta: [cells, genes (oi), knots (32+64+128)]
+        height_delta, overall_delta = self.decoder(latent.to(torch.float), genes_oi)
+
+        # overall_cellxgene: [cells, genes (all)]
+        # cut_localcellxgene_ix: for each cut, the global cell index x local gene index
+        # overall: [cellxgene]
+        overall_baseline = self.overall_baseline.unsqueeze(0)
+        overall_cellxgene = torch.nn.functional.log_softmax(overall_baseline + overall_delta.squeeze(1).transpose(1, 0), -1)
+        overall_likelihood = overall_cellxgene.flatten()[cut_localcellxgene_ix] + math.log(self.n_total_genes)
+
+        # height
+        height_delta = height_delta.permute(2, 0, 1)
+        height_delta_cellxgene = height_delta.reshape(np.prod(height_delta.shape[:2]), height_delta.shape[-1])
+        height_delta = height_delta_cellxgene[cut_local_cellxgene_ix]
+        height_likelihood = self.nf.log_prob(cut_coordinates, genes_oi, cut_local_gene_ix, height_delta)
+
+        # overall likelihood
+        # TODO: check this variable
+        # print(f"{height_likelihood=}")
+        # print(f"{overall_likelihood=}")
+        likelihood = height_likelihood + overall_likelihood
+
+        # ELBO
+        elbo = (-likelihood.sum())
+
+        if return_likelihood:
+            return LikelihoodResult(overall=overall_likelihood, height=height_likelihood, total=likelihood)
+        else:
+            return elbo
+
+    def forward(self, data, return_likelihood=False):
+        print("---  Model.forward()  ---")
+        if not hasattr(data, "latent"):
+            data.latent = self.latent[data.cells_oi]
+        return self.forward_(
+            cut_coordinates=data.cut_coordinates,
+            latent=data.latent,
+            genes_oi=data.genes_oi_torch,
+            cut_local_cellxgene_ix=data.cut_local_cellxgene_ix,
+            cut_local_gene_ix=data.cut_local_gene_ix,
+            cut_localcellxgene_ix=data.cut_localcellxgene_ix,
+            return_likelihood=return_likelihood,
+        )
+    
+    def evaluate_pseudo(self, coordinate_oi, latent_oi, gene_oi=None, gene_ix=None):
+        print("---  Model.evaluate_pseudo()  ---")
+
+        index_tensor = torch.arange(len(latent_oi))
+        index_tensor_repeat = index_tensor.repeat_interleave(len(coordinate_oi))
+
+        index_gene =  index_tensor * self.n_total_genes + gene_oi
+        index_gene_repeat = index_gene.repeat_interleave(len(coordinate_oi))
+
+        coordinate_oi_lt = coordinate_oi.repeat(len(latent_oi))
+        
+        device = coordinate_oi.device # on which device is coordinates stored, other data needs to be on the same device
+
+        if gene_ix is None:
+            if gene_oi is None:
+                gene_oi = 0
+            # single gene provided as integer, convert to tensor
+            genes_oi = torch.tensor([gene_oi], device=device, dtype=torch.long)
+            # tensor with 0, same shape as coordinate_oi, in this case single value
+            cut_local_gene_ix = torch.zeros_like(coordinate_oi_lt).to(torch.long)
+            # tensor with 0, same shape as coordinate_oi, in this case single value
+            cut_local_cellxgene_ix = index_tensor_repeat.to(torch.long)
+            # tensor with 0, same shape as coordinate_oi, in this case single value
+            cut_localcellxgene_ix = index_gene_repeat.to(torch.long)
+
+        data = FullDict(
+            cut_local_gene_ix=cut_local_gene_ix.to(device),
+            cut_local_cellxgene_ix=cut_local_cellxgene_ix.to(device),
+            cut_localcellxgene_ix=cut_localcellxgene_ix.to(device),
+            cut_coordinates=coordinate_oi_lt.to(device),
+            genes_oi_torch=genes_oi.to(device),
+            latent=latent_oi.to(device),
+        )
+
+        with torch.no_grad():
+            result = self.forward(data, return_likelihood=True)
+
+        return result
+
+
+class FullDict(dict):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __str__(self):
+        return str(self.__dict__)
---
+++ src/chromatinhd/models/likelihood_pseudotime/v2/train.py
@@ -0,0 +1,233 @@
+import tqdm.auto as tqdm
+import torch
+import pandas as pd
+from chromatinhd.utils.ansi import colorcodes
+
+
+class Trace:
+    def __init__(self, n_significant_digits=3):
+        self.train_steps = []
+        self.validation_steps = []
+        self.n_current_train_steps = 0
+        self.n_current_validation_steps = 0
+        self.n_last_train_steps = None
+        self.n_last_validation_steps = None
+        self.current_checkpoint = 0
+        self.last_validation_diff = []
+
+    def append(self, loss, epoch, step, phase="train"):
+        if phase == "train":
+            self.train_steps.append(
+                {
+                    "loss": loss,
+                    "epoch": epoch,
+                    "step": step,
+                    "checkpoint": self.current_checkpoint,
+                }
+            )
+            self.n_current_train_steps += 1
+        else:
+            self.validation_steps.append(
+                {
+                    "loss": loss,
+                    "epoch": epoch,
+                    "step": step,
+                    "checkpoint": self.current_checkpoint,
+                }
+            )
+            self.n_current_validation_steps += 1
+
+    def checkpoint(self):
+        if (
+            (self.n_last_train_steps is not None)
+            and (self.n_last_train_steps > 0)
+            and (self.n_current_train_steps > 0)
+        ):
+            last_train_steps = pd.DataFrame(
+                self.train_steps[
+                    -(self.n_current_train_steps + self.n_last_train_steps) : -(
+                        self.n_current_train_steps
+                    )
+                ]
+            )
+            current_train_steps = pd.DataFrame(
+                self.train_steps[-(self.n_current_train_steps) :]
+            )
+
+            current_loss = current_train_steps["loss"].mean()
+            diff_loss = (
+                current_train_steps["loss"].mean() - last_train_steps["loss"].mean()
+            )
+            perc_diff_loss = diff_loss / current_loss
+
+            print(
+                f"{'train':>10} {current_loss:+.2f} Δ{colorcodes.color_sign(diff_loss, '{:+.3f}')} {perc_diff_loss:+.2%}"
+            )
+        self.n_last_train_steps = self.n_current_train_steps
+        self.n_current_train_steps = 0
+
+        if len(self.validation_steps) > 0:
+            current_validation_steps = pd.DataFrame(
+                self.validation_steps[-(self.n_current_validation_steps) :]
+            )
+            current_loss = current_validation_steps["loss"].mean()
+            if (self.n_last_validation_steps is not None) and (
+                self.n_last_validation_steps > 0
+            ):
+                if self.n_current_validation_steps == 0:
+                    raise ValueError(
+                        "No validation steps were run since last checkpoint"
+                    )
+                assert len(self.validation_steps) >= (
+                    self.n_current_validation_steps + self.n_last_validation_steps
+                )
+
+                last_validation_steps = pd.DataFrame(
+                    self.validation_steps[
+                        -(
+                            self.n_current_validation_steps
+                            + self.n_last_validation_steps
+                        ) : -(self.n_current_validation_steps)
+                    ]
+                )
+
+                diff_loss = (
+                    current_validation_steps["loss"].mean()
+                    - last_validation_steps["loss"].mean()
+                )
+                self.last_validation_diff.append(diff_loss)
+                perc_diff_loss = diff_loss / current_loss
+
+                print(
+                    f"{'validation':>10} {current_loss:+.2f} Δ{colorcodes.color_sign(diff_loss, '{:+.3f}')} {perc_diff_loss:+.2%}"
+                )
+            else:
+                print(f"{'validation':>10} {current_loss:+.2f}")
+            self.n_last_validation_steps = self.n_current_validation_steps
+            self.n_current_validation_steps = 0
+
+        self.current_checkpoint += 1
+
+    def plot(self):
+        import matplotlib.pyplot as plt
+
+        fig, ax = plt.subplots()
+        plotdata_validation = (
+            pd.DataFrame(self.validation_steps)
+            .groupby("checkpoint")
+            .mean()
+            .reset_index()
+        )
+        plotdata_train = (
+            pd.DataFrame(self.train_steps).groupby("checkpoint").mean().reset_index()
+        )
+        ax.plot(
+            plotdata_validation["checkpoint"],
+            plotdata_validation["loss"],
+            label="validation",
+        )
+        # ax.plot(plotdata_train["checkpoint"], plotdata_train["loss"], label = "train")
+        ax.legend()
+
+
+class Trainer:
+    def __init__(
+        self,
+        model,
+        loaders,
+        loaders_validation,
+        optim,
+        hooks_checkpoint=None,
+        hooks_checkpoint2=None,
+        device="cuda",
+        n_epochs=30,
+        checkpoint_every_epoch=1,
+        optimize_every_step=1,
+    ):
+        n_steps_total = n_epochs * len(loaders)
+        self.pbar = tqdm.tqdm(total=n_steps_total)
+        self.model = model
+        self.loaders = loaders
+        self.loaders_validation = loaders_validation
+
+        self.trace = Trace()
+
+        self.optim = optim
+
+        self.step_ix = 0
+        self.epoch = 0
+        self.n_epochs = n_epochs
+
+        self.checkpoint_every_epoch = checkpoint_every_epoch
+        self.optimize_every_step = optimize_every_step
+
+        self.device = device
+
+        self.hooks_checkpoint = hooks_checkpoint if hooks_checkpoint is not None else []
+        self.hooks_checkpoint2 = (
+            hooks_checkpoint2 if hooks_checkpoint2 is not None else []
+        )
+
+    def train(self):
+        import gc
+
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        self.model = self.model.to(self.device)
+
+        continue_training = True
+
+        while (self.epoch < self.n_epochs) and (continue_training):
+            self.pbar.set_description(f"epoch {self.epoch}")
+
+            # checkpoint if necessary
+            if (self.epoch % self.checkpoint_every_epoch) == 0:
+                for hook in self.hooks_checkpoint:
+                    hook.start()
+
+                with torch.no_grad():
+                    for data_validation in self.loaders_validation:
+                        data_validation = data_validation.to(self.device)
+
+                        loss = self.model(data_validation).sum()
+
+                        self.trace.append(
+                            loss.item(), self.epoch, self.step_ix, "validation"
+                        )
+
+                        for hook in self.hooks_checkpoint:
+                            hook.run_individual(self.model, data_validation)
+
+                        self.loaders_validation.submit_next()
+                print(f"{'•'} {self.epoch}/{self.n_epochs} {'step':>15}")
+                self.trace.checkpoint()
+
+                for hook in self.hooks_checkpoint:
+                    hook.finish()
+
+                for hook in self.hooks_checkpoint2:
+                    hook.run(self.model)
+
+            # train
+            for data_train in self.loaders:
+                # actual training
+                data_train = data_train.to(self.device)
+
+                loss = self.model.forward(data_train).sum()
+                loss.backward()
+
+                # check if optimization
+                if (self.step_ix % self.optimize_every_step) == 0:
+                    self.optim.step()
+                    self.optim.zero_grad()
+
+                self.step_ix += 1
+                self.pbar.update()
+
+                self.trace.append(loss.item(), self.epoch, self.step_ix, "train")
+
+                self.loaders.submit_next()
+            self.epoch += 1
+
+        self.model = self.model.to("cpu")
