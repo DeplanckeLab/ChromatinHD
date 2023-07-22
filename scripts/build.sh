7c9135daad3c38b727ff7623dd846209085b5e09
Wouter Saelens <wouter.saelens@gmail.com>
2023-07-22 12:24:03 +0200

    add build

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+version="0.0.17"
+
+git add .
+git commit -m "version v${version}"
+
+git tag -a v${version} -m "v${version}"
+
+python -m build
+
+twine upload --repository testpypi dist/chromatinhd-${version}.tar.gz --verbose
+
+
+twine upload dist/chromatinhd-${version}.tar.gz --verbose
