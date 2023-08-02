a1ab7c32f5c21d8ce03b06c2ef6fac47f319513b
Wouter Saelens <wouter.saelens@gmail.com>
2023-08-02 15:23:23 +0200

    update docs

--- src/chromatinhd/models/pred/plot/__init__.py
+++ src/chromatinhd/models/pred/plot/__init__.py
@@ -1,3 +1,5 @@
 from .predictivity import Predictivity, Pileup
 from .effect import Effect
 from .copredictivity import Copredictivity
+
+__all__ = ["Predictivity", "Pileup", "Effect", "Copredictivity"]
