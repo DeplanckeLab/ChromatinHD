438badc059a3f8d247c7f8b868a659a6eeb865b0
Wouter Saelens <wouter.saelens@gmail.com>
2023-08-03 18:40:39 +0200

    Deployed ac22ee2 with MkDocs version: 1.4.3

---
+++ javascripts/reference.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('.doc-attribute>:is(h1, h2, h3, h4, h5, h6)').forEach(function (el) {
+    // add a tag to the attribute at the beginning
+    var tag = document.createElement('span');
+    tag.className = 'doc-attribute-tag';
+    tag.textContent = "attribute"
+
+    el.prepend(tag)
+
+})
+
+document.querySelectorAll('.doc-function>:is(h1, h2, h3, h4, h5, h6)').forEach(function (el) {
+    // add a tag to the function at the beginning
+    var tag = document.createElement('span');
+    tag.className = 'doc-function-tag';
+    tag.textContent = "function"
+
+    el.prepend(tag)
+
+})
+
+
+document.querySelectorAll('.doc-class>:is(h1, h2, h3, h4, h5, h6)').forEach(function (el) {
+    // add a tag to the class at the beginning
+    var tag = document.createElement('span');
+    tag.className = 'doc-class-tag';
+    tag.textContent = "class"
+
+    el.prepend(tag)
+
+})
