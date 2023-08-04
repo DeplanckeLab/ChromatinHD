2aa9f6be0296273b567c0501eaae8e648de2b8e2
Wouter Saelens <wouter.saelens@gmail.com>
2023-08-04 09:24:00 +0200

    test additive model

--- tests/models/pred/model/test_additive.py
+++ tests/models/pred/model/test_additive.py
@@ -1,10 +1,14 @@
-class TestModel:
-    def test_example(self, example_fragments, example_transcriptome):
-        pass
-        # fragments = []
-        # clustering = []
-        # fold = []
-        # model = []
+import chromatinhd as chd
+
+
+class TestAdditive:
+    def test_example(self, example_fragments, example_transcriptome, example_folds):
+        model = chd.models.pred.model.additive.Model(
+            n_genes=len(example_fragments.var.index)
+        )
+        model.train_model(
+            example_fragments, example_transcriptome, fold=example_folds[0], n_epochs=1
+        )
         # result = model.get_prediction(
         #     fragments, clustering, cell_ixs=fold["cells_test"]
         # )
